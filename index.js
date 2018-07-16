35211e56a2253010a27938cafba5968036c2e9b4
Christopher Metzger <chrismetzgr@gmail.com>
2018-07-16 00:52:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(){
-  return Object.assign(driver, { [key] : value })
+  return Object.assign(driver, { [key]: value })
 }
